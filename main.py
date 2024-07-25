58797afd58b669c0a0ffab57ac36df4c14a015b9
Michael Byers <mikebyers24@gmail.com>
2024-07-25 13:12:38 -0400

    refactor with example txt files.

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-with open('Input/Letters/starting_letter.txt', 'r') as letter:
-    letter_contents = letter.read()
-    print(letter_contents)
+with open('Input/Letters/starting_letter.txt', 'r') as letter_file:
+    letter = letter_file.read()
 
-with open('./Input/Companies/companies_list.txt', 'r') as companies:
+with open('./Input/Companies/companies_list.txt', 'r') as companies_file:
     companies_list = []
-    companies_contents = list(companies.readlines())
-    for company in companies_contents:
+    companies = list(companies_file.readlines())
+    for company in companies:
         stripped_companies = company.replace('\n', '')
         companies_list.append(stripped_companies)
 
 for company in companies_list:
     with open('./Output/ReadyToSend/' + company + '.txt', 'w') as company_letters:
-        new_letters = letter_contents.replace('[name]', company)
+        new_letters = letter.replace('[name]', company)
         company_letters.write(new_letters)
